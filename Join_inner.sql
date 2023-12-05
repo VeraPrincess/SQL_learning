8d6a62c2346af27f710aff141c1f3af343935540
Vera Zvereva <veraveraznv@gmail.com>
2023-12-05 20:44:10 +0100

    add Join inner - table with main info
    Join_inner_main_inf.csv

--- Join_inner.sql
+++ Join_inner.sql
@@ -7,3 +7,9 @@
 
 /*Вывести общую таблицу со всеми строками в обеих таблицах*/
 SELECT Hugo_Award_for_Best_Novel.*, authors_books.* FROM Hugo_Award_for_Best_Novel INNER JOIN authors_books ON publisher_id=fk_publisher_id;
+
+/* ALTER TAble authors_books
+RENAME COLUMN title to country */
+
+/*Вывести общую таблицу с нужной информацией*/
+SELECT Hugo_Award_for_Best_Novel.book_title, Hugo_Award_for_Best_Novel.year, authors_books.country, authors_books.author FROM Hugo_Award_for_Best_Novel INNER JOIN authors_books ON publisher_id=fk_publisher_id;
